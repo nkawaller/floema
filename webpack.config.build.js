7f65346725e7f39df0c229c7fc9ba2f53661adc5
nkawaller <63615939+nkawaller@users.noreply.github.com>
2021-05-20 22:29:44 -0400

    webpack plugins

---
+++ webpack.config.build.js
@@ -0,0 +1,7 @@
+const webpack = require("webpack");
+
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev';
+
