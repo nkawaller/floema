fb7fc9b433cce5311148fda2d32cad810de0a5c1
nkawaller <63615939+nkawaller@users.noreply.github.com>
2021-05-23 23:37:58 -0400

    added modules to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -6,38 +6,79 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === "dev";
 
-const dirApp = path.join(__dirname, 'app');
-const dirShared = path.join(__dirname, 'shared');
-const dirStyles = path.join(__dirname, 'styles');
-const dirNode = 'node_modules'
+const dirApp = path.join(__dirname, "app");
+const dirImages = path.join(__dirname, "images");
+const dirShared = path.join(__dirname, "shared");
+const dirStyles = path.join(__dirname, "styles");
+const dirVideos = path.join(__dirname, "videos");
+const dirNode = "node_modules";
 
 module.exports = {
-    entry: [
-        path.join(dirApp, 'index.js'),
-        path.join(dirStyles, 'index.scss')
+  entry: [path.join(dirApp, "index.js"), path.join(dirStyles, "index.scss")],
+
+  resolve: {
+    modules: [dirApp, dirImages, dirShared, dirStyles, dirVideos, dirNode],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT,
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./shared",
+          to: "",
+        },
+      ],
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "",
+            },
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+
+      {
+          test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+          loader: 'file-loader',
+          options: {
+              name (file) {
+                  return '[hash].[ext]'
+              }
+          }
+      }
     ],
-
-    resolve: {
-        modules: [
-            dirApp,
-            dirShared,
-            dirStyles,
-            dirNode
-        ]
-    },
-
-    plugins: [
-        new webpack.DefinePlugin({
-            IS_DEVELOPMENT
-        }),
-
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: './shared',
-                    to: ''
-                }
-            ]
-        })
-    ]
-}
+  },
+};
