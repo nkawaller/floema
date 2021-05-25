5b082045417af58f0920a5e19c9444dc95ab9efa
nkawaller <63615939+nkawaller@users.noreply.github.com>
2021-05-24 22:58:53 -0400

    image minimizer plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,20 @@ const webpack = require("webpack");
 
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === "dev";
 
 const dirApp = path.join(__dirname, "app");
-const dirImages = path.join(__dirname, "images");
 const dirShared = path.join(__dirname, "shared");
 const dirStyles = path.join(__dirname, "styles");
-const dirVideos = path.join(__dirname, "videos");
 const dirNode = "node_modules";
 
 module.exports = {
   entry: [path.join(dirApp, "index.js"), path.join(dirStyles, "index.scss")],
 
   resolve: {
-    modules: [dirApp, dirImages, dirShared, dirStyles, dirVideos, dirNode],
+    modules: [dirApp, dirShared, dirStyles, dirNode],
   },
 
   plugins: [
@@ -37,6 +36,28 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css",
+    }),
+
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        // Lossless optimization with custom option
+        // Feel free to experiment with options for better result for you
+        plugins: [
+          ['gifsicle', { interlaced: true }],
+          ['jpegtran', { progressive: true }],
+          ['optipng', { optimizationLevel: 5 }],
+          [
+            'svgo',
+            {
+              plugins: [
+                {
+                  removeViewBox: false,
+                },
+              ],
+            },
+          ],
+        ],
+      },
     }),
   ],
 
@@ -71,14 +92,14 @@ module.exports = {
       },
 
       {
-          test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
-          loader: 'file-loader',
-          options: {
-              name (file) {
-                  return '[hash].[ext]'
-              }
-          }
-      }
+        test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+        loader: "file-loader",
+        options: {
+          name(file) {
+            return "[hash].[ext]";
+          },
+        },
+      },
     ],
   },
 };
