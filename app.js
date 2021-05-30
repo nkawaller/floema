e4d2109994866263d5fa5f0c0fa892362a1b2e12
nkawaller <63615939+nkawaller@users.noreply.github.com>
2021-05-30 14:50:32 -0400

    link resolver

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const app = express()
 const path = require('path')
 const port = 3000
 
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
+
+// // Link Resolver
+// const handlelinkResolver = doc => {
+//   // Define the url depending on the document type
+//   if (doc.type === 'page') {
+//     return '/page/' + doc.uid
+//   } else if (doc.type === 'blog_post') {
+//     return '/blog/' + doc.uid
+//   }
+
+//   // Default to homepage
+//   return '/'
+// }
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
